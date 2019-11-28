afd62e3ad4057cb647f41c528dcc122aca907be8
Maurilio Silva <Mau@MacBook-Air-de-Maurilio.local>
2019-11-28 20:13:08 -0300

    02 - Método run

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import flask
+from flask import Flask
 # pega o nome do arquivo que está sendo executado
-app = FLASK(__name__) 
+app = Flask(__name__) 
 
 # Utiliza um decorator. A fusão logo abaixo dele.
 @app.route("/")
 def home():
-    return "Hello word!"
+    return "Hello flask 01!"
 
 # Executa o código
-app.run()
+# app.run()
+app.run (debug=True, port=3000, host='0.0.0.0')
