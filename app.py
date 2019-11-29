6c11e4e99a5d4cf0fffa863b98729f948609f8ef
Maurilio Silva <Mau@MacBook-Air-de-Maurilio.local>
2019-11-28 22:27:04 -0300

    13 - Routes

--- app.py
+++ app.py
@@ -1,29 +1,11 @@
 from flask import Flask, request, Response
+import routes
 
 def bootstrap():
     # pega o nome do arquivo que está sendo executado
     app = Flask(__name__) 
 
-    @app.route("/home")
-    def home():
-        # return Response("Home page",200,{})
-        return "Home page",200
-
-    # Utiliza um decorator. A fusão logo abaixo dele.
-    @app.route("/")
-    def showName():
-        #pegar um valor do query string
-        name = request.args.get("name")
-        return f'Name: {name}'
-        # return "Name:"
-
-    @app.route('/name')
-    @app.route('/name/<name>')
-    def showName_param(name = None):
-        if name:
-            return name
-        return "Objeto name não foi definido"
-
+    routes.load(app)
 
     # Retorna o próprio app
     return app
